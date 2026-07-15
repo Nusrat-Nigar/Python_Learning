4c059b1ea0120741466d311cd708b05eb8e2c021
Nusrat Nigar <83997967+Nusrat-Nigar@users.noreply.github.com>
2026-07-15 23:54:13 +0530

    Add files via upload

---
+++ 01_venv.py
@@ -0,0 +1,7 @@
+# v1 - pkg1(pandas)
+# v2 - pkg2(numpy)
+# v3 - pkg3(scipy)
+# ".\env2\Scripts\Activate.ps1" (to activate env2)
+# "pip install -r requirements.txt" (used to install the all pkgs which is mentioned in requirements.txt file )
+
+import moviepy
